e6d447dcd6c81ff8e63f3ff2df083bd93fd66917
hiroyuki1227 <hiroyuki.suda54@gmail.com>
2025-08-16 14:01:07 +0900

    neovim snacks dashboard modify

--- nvim/.config/neobean/lua/plugins/snacks.lua
+++ nvim/.config/neobean/lua/plugins/snacks.lua
@@ -343,36 +343,47 @@ return {
         },
       },
       dashboard = {
-        preset = {
-          keys = {
-            -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            -- {
-            --   icon = " ",
-            --   key = "c",
-            --   desc = "Config",
-            --   action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-            -- },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-            { icon = " ", key = "<esc>", desc = "Quit", action = ":qa" },
-          },
-          -- Font Name: ANSI Shadow
-          -- https://patorjk.com/software/taag
-          header = [[
-███╗   ██╗███████╗ ██████╗ ██████╗ ███████╗ █████╗ ███╗   ██╗
-████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║
-██╔██╗ ██║█████╗  ██║   ██║██████╔╝█████╗  ███████║██╔██╗ ██║
-██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██╔══██║██║╚██╗██║
-██║ ╚████║███████╗╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
-
-[Linkarzu.com]
-        ]],
+        enabled = true,
+        lazy = false,
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
         },
       },
+      --       dashboard = {
+      --         preset = {
+      --           keys = {
+      --             -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      --             -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      --             -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      --             -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      --             -- {
+      --             --   icon = " ",
+      --             --   key = "c",
+      --             --   desc = "Config",
+      --             --   action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+      --             -- },
+      --             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      --             -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      --             { icon = " ", key = "<esc>", desc = "Quit", action = ":qa" },
+      --           },
+      --           -- Font Name: ANSI Shadow
+      --           -- https://patorjk.com/software/taag
+      --           header = [[
+      -- ███╗   ██╗███████╗ ██████╗ ██████╗ ███████╗ █████╗ ███╗   ██╗
+      -- ████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║
+      -- ██╔██╗ ██║█████╗  ██║   ██║██████╔╝█████╗  ███████║██╔██╗ ██║
+      -- ██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██╔══██║██║╚██╗██║
+      -- ██║ ╚████║███████╗╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║
+      -- ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+      --
+      -- [Linkarzu.com]
+      --         ]],
+      --         },
+      --       },
     },
   },
 }
