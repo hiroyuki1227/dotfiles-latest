1826cd9b9f470eea54ca6df3c348d35c2dd85f1e
hiroyuki1227 <hiroyuki.suda54@gmail.com>
2026-09-19 23:16:41 +0900

    modification neovim lsp.lua(craftzdog)

--- nvim/.config/neovim/craftzdog/lua/plugins/lsp.lua
+++ nvim/.config/neovim/craftzdog/lua/plugins/lsp.lua
@@ -138,19 +138,35 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		opts = function()
-			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			vim.list_extend(keys, {
+		opts = {
+			keys = {
 				{
 					"gd",
 					function()
-						-- DO NOT RESUSE WINDOW
+						-- カレントウィンドウで開き、新しいウィンドウにジャンプしない（既存ウィンドウの再利用を無効化）
 						require("telescope.builtin").lsp_definitions({ reuse_win = false })
 					end,
 					desc = "Goto Definition",
 					has = "definition",
 				},
-			})
-		end,
+			},
+		},
 	},
+	--  {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	opts = function()
+	-- 		local keys = require("lazyvim.plugins.lsp.keymaps").get()
+	-- 		vim.list_extend(keys, {
+	-- 			{
+	-- 				"gd",
+	-- 				function()
+	-- 					-- DO NOT RESUSE WINDOW
+	-- 					require("telescope.builtin").lsp_definitions({ reuse_win = false })
+	-- 				end,
+	-- 				desc = "Goto Definition",
+	-- 				has = "definition",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
