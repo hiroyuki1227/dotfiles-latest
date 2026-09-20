2896d696460a7b8e13d952cd842099d4e130a2ea
hiroyuki1227 <hiroyuki.suda54@gmail.com>
2026-09-20 16:44:09 +0900

    modify tmux config

--- nvim/.config/neovim/craftzdog/lua/plugins/colorscheme.lua
+++ nvim/.config/neovim/craftzdog/lua/plugins/colorscheme.lua
@@ -1,30 +1,34 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		branch = "main",
-		lazy = true, -- Load on startup
-		priority = 1000,
-		opts = {
-			transparent = true,
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = true },
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-			sidebars = { "qf", "help" },
-			day_brightness = 0.3,
-			hide_inactive_statusline = false,
-			dim_inactive = false,
-			lualine_bold = false,
-		},
-		-- "craftzdog/solarized-osaka.nvim",
-		-- lazy = true,
-		-- priority = 1000,
-		-- opts = function()
-		-- 	return {
-		-- 		transparent = true,
-		-- 	}
-		-- end,
-	},
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "main",
+    lazy = true, -- Load on startup
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      sidebars = { "qf", "help" },
+      day_brightness = 0.3,
+      hide_inactive_statusline = false,
+      dim_inactive = false,
+      lualine_bold = false,
+      on_highlights = function(hl, c)
+        hl.Pmenu = { fg = c.base0, bg = c.none }
+        hl.BlinkCmpMenu = { fg = c.base01, bg = c.none }
+      end,
+    },
+    -- "craftzdog/solarized-osaka.nvim",
+    -- lazy = true,
+    -- priority = 1000,
+    -- opts = function()
+    -- 	return {
+    -- 		transparent = true,
+    -- 	}
+    -- end,
+  },
 }
